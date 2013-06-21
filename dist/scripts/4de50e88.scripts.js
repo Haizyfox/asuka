bdff28a6563f0d61f785741713d0a43866db4d84
Haizyfox <bradbarrow@gmail.com>
2013-06-21 15:11:16 +1000

    Unignore dist folder - derp

---
+++ dist/scripts/4de50e88.scripts.js
@@ -0,0 +1,1 @@
+"use strict";angular.module("asukaApp",[]).config(["$routeProvider",function(a){a.when("/",{templateUrl:"views/main.html",controller:"MainCtrl"}).otherwise({redirectTo:"/"})}]),angular.module("asukaApp").controller("MainCtrl",["$scope",function(a){a.awesomeThings=["HTML5 Boilerplate","AngularJS","Karma"]}]);
